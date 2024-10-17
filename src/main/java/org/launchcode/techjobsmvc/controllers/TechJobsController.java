7fb015c9d55967dafccfc8441a0075d4ef150872
Rachel Burkhardt <r.burkhardt.ibclc@gmail.com>
2024-10-17 14:49:29 -0500

    Now my Task2Test is failing

---
+++ src/main/java/org/launchcode/techjobsmvc/controllers/TechJobsController.java
@@ -0,0 +1,19 @@
+//package org.launchcode.techjobsmvc.controllers;
+//
+//import org.springframework.ui.Model;
+//
+//import java.util.HashMap;
+//
+//public class TechJobsController {
+//
+//    static HashMap<String, String> actionChoices = new HashMap<>();
+//
+//    public TechJobsController () {
+//        actionChoices.put("search", "Search");
+//        actionChoices.put("list", "List");
+//    }
+//
+//    public getActionChoices(Model model){
+//        model.addAttribute("actions", actionChoices);
+//    }
+//}
