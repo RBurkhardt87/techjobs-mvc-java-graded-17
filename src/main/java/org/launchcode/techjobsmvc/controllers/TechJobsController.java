7eac907010c783336f5ea63d7d4cbe956bfbf7eb
Rachel Burkhardt <r.burkhardt.ibclc@gmail.com>
2024-10-23 12:51:44 -0500

    Got the super bonus done

--- src/main/java/org/launchcode/techjobsmvc/controllers/TechJobsController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/TechJobsController.java
@@ -1,19 +1,63 @@
-//package org.launchcode.techjobsmvc.controllers;
-//
-//import org.springframework.ui.Model;
-//
-//import java.util.HashMap;
-//
-//public class TechJobsController {
-//
-//    static HashMap<String, String> actionChoices = new HashMap<>();
-//
-//    public TechJobsController () {
-//        actionChoices.put("search", "Search");
-//        actionChoices.put("list", "List");
-//    }
-//
-//    public getActionChoices(Model model){
-//        model.addAttribute("actions", actionChoices);
-//    }
-//}
+package org.launchcode.techjobsmvc.controllers;
+
+import org.launchcode.techjobsmvc.models.JobData;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.HashMap;
+
+public class TechJobsController {
+
+    //TODO:create a HashMap called actionChoices
+    static HashMap<String, String> actionChoices = new HashMap<>();
+    static HashMap<String, String> columnChoices = new HashMap<>();
+    static HashMap<String, Object> tableChoices = new HashMap<>();
+
+    //TODO: create an empty constructor
+    //TODO: populate the HashMap with actionChoices within constructor
+    public TechJobsController () {
+        actionChoices.put("search", "Search");
+        actionChoices.put("list", "List");
+
+        columnChoices.put("all", "All");
+        columnChoices.put("employer", "Employer");
+        columnChoices.put("location", "Location");
+        columnChoices.put("positionType", "Position Type");
+        columnChoices.put("coreCompetency", "Skill");
+
+
+        tableChoices.put("employer", JobData.getAllEmployers());
+        tableChoices.put("location", JobData.getAllLocations());
+        tableChoices.put("positionType", JobData.getAllPositionTypes());
+        tableChoices.put("coreCompetency", JobData.getAllCoreCompetency());
+    }
+
+    //TODO: write a static method that returns the actionChoice HashMap
+    //TODO: add annotation @ModelAttribute("actions") to the method
+    //this annotation will cause the return value of the method to be set in the model with "actions" as the key
+    //for every controller that extends TechJobsController
+    @ModelAttribute("actions")
+    private static HashMap<String, String> getActionChoices(){
+        return actionChoices;
+    }
+
+    @ModelAttribute("columns")
+    private static HashMap<String, String> getColumnChoices(){
+        return columnChoices;
+    }
+
+    @ModelAttribute("tables")
+    private static HashMap<String, Object> getTableChoices(){
+        return tableChoices;
+    }
+
+
+
+
+
+
+
+
+
+
+}
